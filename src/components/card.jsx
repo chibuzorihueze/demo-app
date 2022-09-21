067851fcb87421fcb3ee164cd7c1b4ddebc4bf14
Chibuzor Ihueze <krisykord@gmail.com>
2022-09-21 05:01:45 +0100

    re-arranging components in a directory

---
+++ src/components/card.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Contact from "./contact";
+
+function Card(){
+    return(
+        <div>
+            <h1>Contact App</h1>
+      <Contact 
+
+      name ="Jose Mourinho"
+      img = "https://img.a.transfermarkt.technology/portrait/header/781-1488880368.jpg?lm=1"
+      tel = "tel : +246677324"
+      email = "email: josemourinho@gmail.com"
+      alt ="Jose Mourinho"
+      />
+
+      <Contact
+
+      name = "Denzel Washington"
+      img ="https://st.depositphotos.com/1814084/1709/i/450/depositphotos_17095517-stock-photo-denzel-washington.jpg"
+      tel = "tel : +13323444334"
+      email = "email: denzelwashington@gmail.com"
+      alt = "Denzel Washington"
+      
+      />
+
+      <Contact
+      name = "Chris Oyakilome"
+      img = "https://christembassynungua.org/wp-content/uploads/2016/06/pastor-chris-oyakhilome-christ-embassy-church.jpg"
+      tel = "tel: +23486788999"
+      email ="email: chrisoyakilome@gmail.com"
+      alt ="Chris Oyakilome"
+      />
+    </div>
+    
+    )
+}
+
+export default Card;
