51b0fe278af68976ea6728b7f2ac8c3609e4ed68
Chibuzor Ihueze <krisykord@gmail.com>
2022-09-21 03:56:42 +0100

    template to practice props

---
+++ src/components/contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact(){
+    return(
+        <div>
+            <img src="https://img.a.transfermarkt.technology/portrait/header/781-1488880368.jpg?lm=1" alt="jose mourinho" />
+            <p>contact: +246677324</p>
+            <p>email: josemourinho@gmail.com</p>
+        </div>
+    )
+}
+
+export default Contact;
