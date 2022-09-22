f8e35bbf21a9bb7db1217df8e23e3a232ae2b307
Chibuzor Ihueze <krisykord@gmail.com>
2022-09-22 03:22:12 +0100

    update contact with css

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,13 +4,13 @@ import Contact from "./contact";
 function Card(){
     return(
         <div>
-            <h1>Contact App</h1>
+            <h1 className="heading">Contact App</h1>
       <Contact 
 
       name ="Jose Mourinho"
       img = "https://img.a.transfermarkt.technology/portrait/header/781-1488880368.jpg?lm=1"
-      tel = "tel : +246677324"
-      email = "email: josemourinho@gmail.com"
+      tel = "+24667732684"
+      email = "josemourinho@gmail.com"
       alt ="Jose Mourinho"
       />
 
@@ -18,8 +18,8 @@ function Card(){
 
       name = "Denzel Washington"
       img ="https://st.depositphotos.com/1814084/1709/i/450/depositphotos_17095517-stock-photo-denzel-washington.jpg"
-      tel = "tel : +13323444334"
-      email = "email: denzelwashington@gmail.com"
+      tel = "+13323444334"
+      email = "denzelwashington@gmail.com"
       alt = "Denzel Washington"
       
       />
@@ -28,7 +28,7 @@ function Card(){
       name = "Chris Oyakilome"
       img = "https://christembassynungua.org/wp-content/uploads/2016/06/pastor-chris-oyakhilome-christ-embassy-church.jpg"
       tel = "tel: +23486788999"
-      email ="email: chrisoyakilome@gmail.com"
+      email ="chrisoyakilome@gmail.com"
       alt ="Chris Oyakilome"
       />
     </div>
